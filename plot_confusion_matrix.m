b2158fe69e98beefe2715dc53d9969f0083c5894
Zhao Koh <zhao.koh@gmail.com>
2018-03-05 11:06:13 +1100

    Refactored plot confusion matrix to selectdata for reusability

---
+++ plot_confusion_matrix.m
@@ -0,0 +1,41 @@
+function plot_confusion_matrix(experiment, scoredTrain, predictTrain, scoredTest, predictTest, cm_save_dir)
+    %% Confusion matrix of train data
+    % Reshape scored and predict matrix
+    g_labelTrain = reshape(scoredTrain,1,[]);
+    g_clustTrain = reshape(predictTrain,1,[]);
+
+    % Labelled - make non-zero stage
+    g_labelTrain = g_labelTrain+1;
+
+    % Clustered - Use final clustering output
+    g_clustTrain = g_clustTrain+1;
+
+    % Cluster 6 becomes 5
+    g_labelTrain(g_labelTrain==6) = 5;
+    g_clustTrain(g_clustTrain==6) = 5;
+
+    % Visualise confusion matrix
+    figure;
+    plotconfusion_custom(g_labelTrain, g_clustTrain, 'Confusion Matrix - Training');
+    saveas(gcf, strcat(cm_save_dir, filesep, 'CM_TRN_', int2str(experiment), '.png'));
+
+    %% Confusion matrix of test data
+    % Reshape scored and predict matrix
+    g_labelTest = reshape(scoredTest,1,[]);
+    g_clustTest = reshape(predictTest,1,[]);
+
+    % Labelled - make non-zero stage
+    g_labelTest = g_labelTest+1;
+
+    % Clustered - Use final clustering output
+    g_clustTest = g_clustTest+1;
+
+    % Cluster 6 becomes 5
+    g_labelTest(g_labelTest==6) = 5;
+    g_clustTest(g_clustTest==6) = 5;
+
+    % Visualise confusion matrix
+    figure;
+    plotconfusion_custom(g_labelTest, g_clustTest, 'Confusion Matrix - Testing');
+    saveas(gcf, strcat(cm_save_dir, filesep, 'CM_TST_', int2str(experiment), '.png'));
+end
