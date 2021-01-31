828ad44d6d70a070b2cd9931992393c9428fb4ef
NicolasDecat <nicolas.decat@studenti.unitn.it>
2021-01-31 23:19:03 +0100

    update

--- hctsa_scripts/Plots_AccuracyMatrix.m
+++ hctsa_scripts/Plots_AccuracyMatrix.m
@@ -539,7 +539,7 @@ ylabel('Percentage Accuracy')
 % 
 % TimeSeries.Data = Data;
 
-%% on obtaining Per_correct_mean_D
+%% on obtaining Per_correct_mean_D  /   or Per_correct_mean_D_SVM
 
 % Copy pasted script to get 7749-matrix
 load('/Users/nico/Documents/HCTSA/Analysis/Accuracy/Matrix_accuracy_per_feat/All_unique_specificity_feat_combined')  % all specifically removed featured across datasets (combined ('unique'))
@@ -579,9 +579,17 @@ for D = 1:length(Subs)
     end
     
     % Store
-    Per_correct_mean_D{D} = Per_correct_mean;
+    Per_correct_mean_D_SVM{D} = Per_correct_mean;
     
 end
+
+%% SVM 
+
+%%% 1) Section above ("on obtaining Per_correct_mean_D / or
+%%% Per_correct_mean_D_SVM") to get Per_correct_mean_D_SVM
+%%% 2) Remove SV features:
+%%% Per_correct_mean_D_SVM{1,1}(:,spec_and_common_feat) = []; to get
+%%% 10x5308 matrix
 
 
 %% Top 40 features for one Dataset (only WB Features)
@@ -609,7 +617,7 @@ for D = 1:length(Subs)
     
 end
 
-Per_correct_mean = Per_correct_mean_D_excl{y};
+Per_correct_mean = Per_correct_mean_D_SVM{y};
 
 % Reorder features: from yielding theg highest to lowest accuracy
 means = mean(Per_correct_mean);
@@ -778,7 +786,7 @@ numTopFeatures = 40;  % Number of top features
 op_ind = cell2mat(Top_ID'); % indices of top 40 features (among the 7k)
 
 % Compute correlations based on hctsa responses
-HCTSA_N = true;   % true if want to plot normalized hctsa values; else plot HCTSA.mat values
+HCTSA_N = false;   % true if want to plot normalized hctsa values; else plot HCTSA.mat values
 
 if HCTSA_N
     EEGonly = 1:size(TS_DataMat,1)/7;
@@ -959,7 +967,7 @@ wake = 0; N1 = 1; N2 = 3; N3 = 3; rem = 5;
 CLASSIFIER = {[wake,N1] [wake,N2] [wake,N3] [wake,rem] [N1,N2] [N1,N3] [N1,rem] [N2,N3] [N2,rem] [N3,rem]};
 
 % Which classifier do we want to plot?
-ClNum = 3;  % Wake vs N1
+ClNum = 10;  
 Classif = Top_40{1,ClNum};  % Get the top features of the chosen classifier
 Top_Feat = Top_Feats{1,ClNum};
 
@@ -1111,7 +1119,7 @@ end
 
 % load Matrix with TopFeat for each classifier and dataset
 load('/Users/nico/Documents/HCTSA/Analysis/Accuracy/Matrix_accuracy_per_feat/Top_Feat_Data_Class')  % all specifically removed featured across datasets (combined ('unique'))
-whichClassif = 1;
+whichClassif = 2;
 
 
 % Get the top Feat ID of a classifier across datasets
@@ -1138,24 +1146,39 @@ Top_Elem = [UniqueElem SumElem];
 Top_Elem = Top_Elem(I,:);
 UniqueElem = UniqueElem(I,:);
 
-% Get the keywords assigned to Feat ID
-load('HCTSA.mat','Operations');
-Keywords = {Operations.Keywords}.';
-CodeString = {Operations.CodeString}.';
+load('/Users/nico/Documents/HCTSA/Analysis/Accuracy/Matrix_accuracy_per_feat/Matrix_excl_all_feat_removed(5308)_all_datasets')
+load('/Users/nico/Documents/HCTSA/Analysis/Accuracy/Matrix_accuracy_per_feat/Per_correct_mean_D_excl')
+load('/Users/nico/Documents/HCTSA/Analysis/Accuracy/Matrix_accuracy_per_feat/ALL_removed_feat(2441)')
 
-Top_Keywords = Keywords(Top_Elem(:,1));
-Top_name = CodeString(Top_Elem(:,1));
+load('HCTSA.mat', 'Operations')   % Simply to get the list of 7749 features 
+
+equi_Top_Feat = setdiff(1:7749,spec_and_common_feat); % get the equivalent ranking of top feat after special-value features removed
+Operations = Operations(equi_Top_Feat,:);  % get Operations of well-behaved features only
+
+CodeString = {Operations.CodeString}.';  
+Keywords = {Operations.Keywords}.';
+YLabel = {Operations.ID}.';
+
+
+% Convert 7749-based index (UniqueElem) to 5308-based index (Idx_UniqueElem)
+
+for i = 1:length(UniqueElem)
+    Idx_UniqueElem(i,:) = find(cell2mat(YLabel) == UniqueElem(i));
+end
+
+Top_Keywords = Keywords(Idx_UniqueElem(:,1));
+Top_name = CodeString(Idx_UniqueElem(:,1));
 Top_Keywords = [Top_name Top_Keywords num2cell(Top_Elem)];   % This top features may include features that are top in some datasets and special-value features in other datasets (that's why mean accuracy can be super low (around 7% if 11 datasets produced special values))
 
 
 % Get % accuracy for each top feat (average of accuracy from ALL datasets)
 Accuracy = [];
-load('/Users/nico/Documents/HCTSA/Analysis/Accuracy/Matrix_accuracy_per_feat/Per_correct_mean_D')
+load('/Users/nico/Documents/HCTSA/Analysis/Accuracy/Matrix_accuracy_per_feat/Per_correct_mean_D_excl')
 
 for ID = 1:length(UniqueElem)
     
     for D = 1:12
-        Accuracy(D) = Per_correct_mean_D{1,D}(whichClassif,UniqueElem(ID));
+        Accuracy(D) = Per_correct_mean_D_excl{1,D}(whichClassif,Idx_UniqueElem(ID));
     end
     
     Top_Keywords(ID,5) = {mean(Accuracy)}; 
@@ -1178,32 +1201,37 @@ Top_ID = Top_Keywords(:,3);
 
 %%% Highlight which top feature was selected from AveragedMatrix
 
-load('/Users/nico/Documents/HCTSA/Analysis/Accuracy/Matrix_accuracy_per_feat/Matrix_incl_all_feat_removed(7749)_all_datasets')
+% Matrices with only WB features
+load('/Users/nico/Documents/HCTSA/Analysis/Accuracy/Matrix_accuracy_per_feat/Matrix_excl_all_feat_removed(5308)_all_datasets')
+load('/Users/nico/Documents/HCTSA/Analysis/Accuracy/Matrix_accuracy_per_feat/Per_correct_mean_D_excl')
+load('/Users/nico/Documents/HCTSA/Analysis/Accuracy/Matrix_accuracy_per_feat/ALL_removed_feat(2441)')
 
-% Load Data
-load HCTSA.mat
+% Get 'Operations of WB features only
+load('HCTSA.mat', 'Operations')  
 
-% Get top features per classifier
+equi_Top_Feat = setdiff(1:7749,spec_and_common_feat);     % remove SV features
+Operations = Operations(equi_Top_Feat,:);                 % 'Operations' with WB features only (from HCTSA_N)
 
 CodeString = {Operations.CodeString}.';  
 Keywords = {Operations.Keywords}.';
 YLabel = {Operations.ID}.';
 
 for C = 1:10
-
-    % Features reordering: from best to worst feature
-    [~,I] = sort(AveragedMatrix(C,:)','descend');
-    Top_Feat_aver = I(1:40);
-
+    
+    % Reorder features: from yielding theg highest to lowest accuracy
+    [~,I] = sort(AveragedMatrix_excl(C,:)','descend');
+    Top_Feats{C} = I(1:40);   
+                     
     % Get the name and keyword associated with these features
-    Top_name_aver(1:40) = CodeString(Top_Feat_aver,1);
-    Top_key_aver(1:40) = Keywords(Top_Feat_aver,1);
-    Top_ID_aver(1:40) = YLabel(Top_Feat_aver,1);
-    Top_mean_aver(1:40) = AveragedMatrix(C,Top_Feat_aver);
+    Top_name_aver(1:40) = CodeString(Top_Feats{C},1);
+    Top_key_aver(1:40) = Keywords(Top_Feats{C},1);
+    Top_ID_aver(1:40) = YLabel(Top_Feats{C},1);
+    Top_mean_aver(1:40) = AveragedMatrix_excl(C,Top_Feats{C});
 
     Top_40_aver{C} = [Top_name_aver' Top_key_aver' Top_ID_aver' num2cell(Top_mean_aver')];
 
 end
+
 
 % Top_features of corresponding classifier; used in AverageMatrix
 Top_aver = Top_40_aver{1,whichClassif};
@@ -1232,6 +1260,12 @@ ii = bar(Featmode(selected_feat_aver));  % Features selected by AveragedMatrix a
 set(ii,'FaceColor','r');
 hold off
 
+% To check why features from AveragedMatrix_excl are not in any top 40
+% feature of any dataset
+% for D = 1:12
+% X(D) = Per_correct_mean_D_excl{1,D}(whichClassif,find(cell2mat(YLabel) == 6660));  % F being a feature in top_40 aver that is not found in UniqueElem
+% end
+% mean(X)
 
 
 %%%%% Try something similar, but instead of plotting ind features; let's plot the top families
@@ -1260,5 +1294,25 @@ UniqueKey = [UniqueKey num2cell(Mean_acc) num2cell(UniqueKey_rep)];   % 3rd col 
 % corresponding features)
 [~,I] = sort((Mean_acc)','descend');
 UniqueKey = UniqueKey(I,:);
+
+
+% Plot distribution of top 20 families across datasets
+Fammode = cell2mat(UniqueKey(1:20,3));   % y axis bar(Fammode)
+Families = UniqueKey(1:20,1)';
+
+g = bar(Fammode);
+
+ax = gca;
+ax.XTickLabels = arrayfun(@(a)string(a),Families(:,1:20),'uni',0);
+xticks(1:length(Families))
+xtickangle(35)
+ylim([0 max(Fammode)+1])
+xlabel('Top 20 families across all datasets (ranked from family yielding highest to lowest accuracy) ')
+ylabel('Number of datasets sharing one top feature of the family')
+title('Distribution of top families across datasets')
+
+
+
+
 
 
